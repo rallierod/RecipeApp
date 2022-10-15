6c4a80243530946b736ab8335f633e9f2dbcb092
rallierod <99930994+rallierod@users.noreply.github.com>
2022-10-15 12:24:08 -0500

    added multiple components

---
+++ recipeApp/src/app/add-new-recipe/add-new-recipe.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-recipe',
+  templateUrl: './add-new-recipe.component.html',
+  styleUrls: ['./add-new-recipe.component.css']
+})
+export class AddNewRecipeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- recipeApp/src/app/app.module.ts
+++ recipeApp/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { AddNewRecipeComponent } from './add-new-recipe/add-new-recipe.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AddNewRecipeComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
